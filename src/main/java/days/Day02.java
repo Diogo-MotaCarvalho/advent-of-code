4cec8048830f2c3fee00f78cf1070df6c35f9be6
Diogo-MotaCarvalho <diogo.carvalho@flutteruki.com>
2024-12-04 17:19:56 +0000

    Day2 part 2 complete

--- src/main/java/days/Day02.java
+++ src/main/java/days/Day02.java
@@ -8,16 +8,7 @@ public class Day02 implements Day {
     @Override
     public int part1(List<String> strings) {
 
-        Set<List<Integer>> reports = new HashSet<>();
-
-        for (String s : strings) {
-            List<Integer> row = new ArrayList<>();
-            String [] split = s.split(" ");
-            for(String str : split) {
-                row.add(Integer.parseInt(str));
-            }
-            reports.add(row);
-        }
+        Set<List<Integer>> reports = populate(strings);
 
         int safeCount = 0;
 
@@ -51,7 +42,54 @@ public class Day02 implements Day {
 
     @Override
     public int part2(List<String> strings) {
-        return 0;
+        Set<List<Integer>> reports = populate(strings);
+
+        int safeCount = 0;
+
+        for (List<Integer> row : reports) {
+            int dif;
+            boolean flag = true;
+            boolean flag2;
+            boolean decreasing;
+            int count=0;
+
+            decreasing = row.get(0) < row.get(1);
+
+            for(int i = 0; i < row.size()-1; i++) {
+
+
+                flag2 = row.get(i) < row.get(i+1);
+
+                dif= Math.abs(row.get(i) - row.get(i+1));
+
+                if (dif > 3 || decreasing!=flag2 || dif==0) {
+                    if(count==1){
+                        count++;
+                        break;
+                    }
+                    flag = false;
+                    count++;
+                }
+            }
+            if (flag || count <=1) {
+                safeCount++;
+            }
+        }
+
+        return safeCount;
+    }
+
+    private Set<List<Integer>> populate(List<String> strings) {
+        Set<List<Integer>> reports = new HashSet<>();
+        for (String s : strings) {
+            List<Integer> row = new ArrayList<>();
+            String [] split = s.split(" ");
+            for(String str : split) {
+                row.add(Integer.parseInt(str));
+            }
+            reports.add(row);
+        }
+        return reports;
     }
 
 }
