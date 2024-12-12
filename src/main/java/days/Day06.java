b2b3354fbce4c12d1acc68748cba8c531e9008f1
Diogo-MotaCarvalho <diogo.carvalho@flutteruki.com>
2024-12-12 17:25:28 +0000

    Day06 part1 complete

--- src/main/java/days/Day06.java
+++ src/main/java/days/Day06.java
@@ -2,26 +2,29 @@ package days;
 
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day06 implements Day {
-
+    List<List<Integer>> visited = new ArrayList<>();
 
     @Override
     public int part1(List<String> strings) {
         List<List<Character>> matrix = getMap(strings);
-        List<List<Integer>> visited = new ArrayList<>();
         List<Integer> startingPosition = startingPosition(matrix);
         List<Integer> direction = new ArrayList<>();
         direction.add(-1);
         direction.add(0);
-        for (int i = startingPosition.getFirst(); i < matrix.size(); ) {
-            for (int j = startingPosition.getLast(); j < matrix.get(i).size() ; ) {
+        for (int i = startingPosition.getFirst(); ; ) {
+            for (int j = startingPosition.getLast(); ; ) {
+                //stop conditions
+                if (i >= matrix.size() || j >= matrix.get(0).size() || i < 0 || j < 0) {
+                    return removeDuplicates(visited).size();
+                }
                 //turn 90 degrees right
                 if (matrix.get(i).get(j) == '#') {
-                    List<Integer> newDirection = turn(visited);
-                    direction.set(0, newDirection.getFirst());
-                    direction.set(1, newDirection.getLast());
+                    direction = turn(visited);
                     i = Integer.sum(visited.getLast().getFirst(), direction.getFirst());
                     j = Integer.sum(visited.getLast().getLast(), direction.getLast());
                 } else {//Keeps moving and adds position to visited positions
@@ -34,13 +37,34 @@ public class Day06 implements Day {
                 }
             }
         }
-        return 0;
     }
 
     @Override
     public int part2(List<String> strings) {
-        return 0;
+        List<List<Integer>> visitedNoDuplicates = removeDuplicates(visited);
+        int count = 0;
+        for (int i = 0; i < visitedNoDuplicates.size(); i++) {
+            for (int j = 0; j < visitedNoDuplicates.get(i).size(); j++) {
+                if (visitedNoDuplicates.get(i).getFirst() == visitedNoDuplicates.get(j).getFirst()) {
+                    for (int k = 0; k < visitedNoDuplicates.get(i).size(); k++) {
+                        if (visitedNoDuplicates.get(j).getLast() == visitedNoDuplicates.get(k).getLast()) {
+                            for (int l = 0; l < visitedNoDuplicates.get(i).size(); l++) {
+                                if (visitedNoDuplicates.get(l).getFirst() == visitedNoDuplicates.get(k).getFirst() && visitedNoDuplicates.get(l).getLast() == visitedNoDuplicates.get(i).getLast()) {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
     }
+    //[0][1] [0][2] [0][3]
+    //[1][1] [1][2] [1][3]
+    //[2][1] [2][2] [2][3]
+    //[3][1] [3][2] [3][3]
 
 
     private List<List<Character>> getMap(List<String> strings) {
@@ -78,11 +102,11 @@ public class Day06 implements Day {
 
         if (lastLastVisited.getFirst() - lastVisited.getFirst() == -1) {
             turn.add(0);
-            turn.add(1);
+            turn.add(-1);
         }
         if (lastLastVisited.getFirst() - lastVisited.getFirst() == 1) {
             turn.add(0);
-            turn.add(-1);
+            turn.add(1);
         }
         if (lastLastVisited.getLast() - lastVisited.getLast() == -1) {
             turn.add(1);
@@ -94,4 +118,10 @@ public class Day06 implements Day {
         }
         return turn;
     }
+
+    private <T> List<T> removeDuplicates(List<T> list) {
+        Set<T> set = new HashSet<>(list);
+        return new ArrayList<>(set);
+    }
+
 }
